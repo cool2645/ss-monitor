805ae519a84630b73fc1fd5510f404160d20e6a0
LI JIAHAO <lijiahao99131@gmail.com>
2018-01-29 13:11:00 +0800

    add manager

--- config/config.go
+++ config/config.go
@@ -3,15 +3,18 @@ package config
 var GlobCfg = Config{}
 
 type Config struct {
-	PORT         int64    `toml:"port"`
-	ALLOW_ORIGIN []string `toml:"allow_origin"`
-	API_KEY      string   `toml:"api_key"`
-	TG_KEY       string   `toml:"tg_key"`
-	DB_NAME      string   `toml:"db_name"`
-	DB_USER      string   `toml:"db_user"`
-	DB_PASS      string   `toml:"db_pass"`
-	DB_CHARSET   string   `toml:"db_charset"`
-	ADMIN        []Admin  `toml:"admin"`
+	PORT             int64    `toml:"port"`
+	ALLOW_ORIGIN     []string `toml:"allow_origin"`
+	API_KEY          string   `toml:"api_key"`
+	TG_KEY           string   `toml:"tg_key"`
+	HEARTBEAT_ALARM  int64    `toml:"heartbeat_alarm_val"`
+	MONITOR_INTERVAL int64    `toml:"monitor_interval"`
+	MANAGER_NAME     string   `toml:"manager_name"`
+	DB_NAME          string   `toml:"db_name"`
+	DB_USER          string   `toml:"db_user"`
+	DB_PASS          string   `toml:"db_pass"`
+	DB_CHARSET       string   `toml:"db_charset"`
+	ADMIN            []Admin  `toml:"admin"`
 }
 
 type Admin struct {
---
+++ manager/manager.go
@@ -0,0 +1,150 @@
+package manager
+
+import (
+	"github.com/cool2645/ss-monitor/model"
+	. "github.com/cool2645/ss-monitor/config"
+	"time"
+	"github.com/pkg/errors"
+	"sync"
+	"fmt"
+	"github.com/cool2645/ss-monitor/broadcaster"
+)
+
+var mux sync.Mutex
+var workers = make(map[string]map[string]model.Heartbeat)
+
+func Init() {
+	workers["watcher"] = make(map[string]model.Heartbeat)
+	workers["ipv4tester"] = make(map[string]model.Heartbeat)
+	workers["ipv6tester"] = make(map[string]model.Heartbeat)
+	workers["cleaner"] = make(map[string]model.Heartbeat)
+	heartbeats, err := model.GetHeartbeats(model.Db, time.Now().Unix()-GlobCfg.HEARTBEAT_ALARM, "watcher", 10)
+	if err != nil {
+		panic(err)
+		return
+	}
+	for _, v := range heartbeats {
+		workers["watcher"][v.Name] = v
+	}
+	heartbeats, err = model.GetHeartbeats(model.Db, time.Now().Unix()-GlobCfg.HEARTBEAT_ALARM, "tester", 4)
+	if err != nil {
+		panic(err)
+		return
+	}
+	for _, v := range heartbeats {
+		workers["ipv4tester"][v.Name] = v
+	}
+	heartbeats, err = model.GetHeartbeats(model.Db, time.Now().Unix()-GlobCfg.HEARTBEAT_ALARM, "tester", 6)
+	if err != nil {
+		panic(err)
+		return
+	}
+	for _, v := range heartbeats {
+		workers["ipv6tester"][v.Name] = v
+	}
+	heartbeats, err = model.GetHeartbeats(model.Db, time.Now().Unix()-GlobCfg.HEARTBEAT_ALARM, "cleaner", 10)
+	if err != nil {
+		panic(err)
+		return
+	}
+	for _, v := range heartbeats {
+		workers["cleaner"][v.Name] = v
+	}
+	go monitorWorkers()
+}
+
+func monitorWorkers() {
+	for {
+		checkWorkerStatus()
+		time.Sleep(time.Second * time.Duration(GlobCfg.MONITOR_INTERVAL))
+	}
+}
+
+func ReportWorkerStatus() (map[string]map[string]model.Heartbeat) {
+	return workers
+}
+
+func UpdateWorkerStatus(class string, ipVer uint, name string) (heartbeat model.Heartbeat, err error) {
+	var workerClassKey []string
+	switch class {
+	case "tester":
+		if ipVer == 4 {
+			workerClassKey = append(workerClassKey, "ipv4tester")
+		} else if ipVer == 6 {
+			workerClassKey = append(workerClassKey, "ipv6tester")
+		} else {
+			workerClassKey = append(workerClassKey, "ipv4tester")
+			workerClassKey = append(workerClassKey, "ipv6tester")
+		}
+	default:
+		workerClassKey = append(workerClassKey, class)
+	}
+	t := time.Now().Unix()
+	var worker model.Heartbeat
+	isNewWorker := false
+	for _, wck := range workerClassKey {
+		if val, ok := workers[wck][name]; ok {
+			worker = val
+		} else {
+			worker.Name = name
+			isNewWorker = true
+		}
+		worker.Class = class
+		worker.IPVer = ipVer
+		worker.Time = t
+		workers[wck][name] = worker
+	}
+	heartbeat, err = model.SaveHeartbeat(model.Db, class, ipVer, name, t)
+	if err != nil {
+		err = errors.Wrap(err, "UpdateWorkerStatus")
+		return
+	}
+	if isNewWorker {
+
+	}
+	return
+}
+
+func checkWorkerStatus() {
+	mux.Lock()
+	defer mux.Unlock()
+	for i, ws := range workers {
+		var offlineList []model.Heartbeat
+		for k, v := range ws {
+			if v.Time < time.Now().Unix()-GlobCfg.HEARTBEAT_ALARM {
+				offlineList = append(offlineList, v)
+				delete(workers[i], k)
+			}
+		}
+		if len(offlineList) != 0 || len(workers[i]) == 0 {
+			alert(offlineList, workers[i], i)
+		}
+	}
+}
+
+func singleOrPlural(cnt int) (string) {
+	if cnt > 0 {
+		return "s"
+	} else {
+		return ""
+	}
+}
+
+func alert(offlineWorkers []model.Heartbeat, onlineWorkers map[string]model.Heartbeat, wck string) {
+	var msg string
+	if len(offlineWorkers) > 0 {
+		msg += fmt.Sprintf("*🔴 %d %s%s get offline*\n", len(offlineWorkers), wck, singleOrPlural(len(offlineWorkers)))
+	}
+	for _, v := range offlineWorkers {
+		msg += fmt.Sprintf("  🔴 %s: Last heartbeat at %s\n", v.Name, time.Unix(v.Time, 0).Format("15:04"))
+	}
+	if len(onlineWorkers) == 0 {
+		msg += fmt.Sprintf("*🔴 %d %s%s currently online*\n", len(onlineWorkers), wck, singleOrPlural(len(onlineWorkers)))
+	} else {
+		msg += fmt.Sprintf("*🔵 %d %s%s currently online*\n", len(onlineWorkers), wck, singleOrPlural(len(onlineWorkers)))
+	}
+	for _, v := range onlineWorkers {
+		msg += fmt.Sprintf("  🔵 %s: Last heartbeat at %s\n", v.Name, time.Unix(v.Time, 0).Format("15:04"))
+	}
+	broadcaster.Broadcast(msg, GlobCfg.MANAGER_NAME, "manager")
+}
